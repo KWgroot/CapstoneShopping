90185667c9fbc20adb68fc422f4ae5ddcad5eaaf
Koen Groot <kwgroot@gmail.com>
2021-01-03 17:46:06 +0100

    App functions, but when I use the save Fab the app crashes, looking into that now.

--- app/src/main/java/com/example/capstoneshopping/UI/GroceryListFragment.kt
+++ app/src/main/java/com/example/capstoneshopping/UI/GroceryListFragment.kt
@@ -1,7 +1,77 @@
 package com.example.capstoneshopping.UI
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.capstoneshopping.R
+import com.example.capstoneshopping.Viewmodel.Grocery
+import com.example.capstoneshopping.Viewmodel.GroceryViewModel
+import kotlinx.android.synthetic.main.fragment_fridgelist.*
 
 class GroceryListFragment : Fragment(){
+    private lateinit var groceryAdapter: GroceryAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private var groceries: ArrayList<Grocery> = arrayListOf()
+    private val viewModel: GroceryViewModel by viewModels()
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_fridgelist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRv()
+        observeAddGrocery()
+    }
+
+    private fun observeAddGrocery(){
+        viewModel.groceries.observe(viewLifecycleOwner, Observer { groceries ->
+            this.groceries.clear()
+            this.groceries.addAll(groceries)
+            this.groceries.sortBy { it.date }
+
+            groceryAdapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun initRv(){
+        groceryAdapter = GroceryAdapter(groceries)
+        viewManager = LinearLayoutManager(activity)
+
+        createItemTouchHelper().attachToRecyclerView(rvGroceryList)
+
+        rvGroceryList.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = groceryAdapter
+        }
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper{
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val groceryToDelete = groceries[position]
+                viewModel.deleteGrocery(groceryToDelete)
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
 }
--- app/src/main/java/com/example/capstoneshopping/UI/MainActivity.kt
+++ app/src/main/java/com/example/capstoneshopping/UI/MainActivity.kt
@@ -6,9 +6,55 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.capstoneshopping.R
+import com.example.capstoneshopping.Viewmodel.GroceryViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private val viewModel: GroceryViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
+        navController = findNavController(R.id.nav_host_fragment)
+        addFab.setOnClickListener {
+            navController.navigate(
+                R.id.action_groceryListFragment_to_addGroceryFragment
+            )
+        }
+        fabToggler()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.bin -> {
+                viewModel.deleteAllGroceries()
+                Toast.makeText(applicationContext, R.string.deleted_groceries, Toast.LENGTH_SHORT).show()
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun fabToggler(){
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.addGroceryFragment)){
+                addFab.hide()
+            }else{
+                addFab.show()
+            }
+        }
+    }
 }
